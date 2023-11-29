9dc3446c14d4ac7cdb68e16e786c0f91e0b025e6
lbarberena <octaviobarberena@hotmail.com>
2023-11-29 01:44:12 +0100

    Edit main.py to make it accessible as API

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ import requests
 import pandas as pd
 from parsers.clean_date import clean_date
 from models.patents_md import Patent
+from flask import Flask, send_file
 
-response = requests.get("https://data.nasa.gov/resource/gquh-watm.json")
-data = response.json()
+app = Flask(__name__)
 
-new_data = clean_date(data)
+@app.route('/data', methods=['GET'])
+def get_data():
+    response = requests.get("https://data.nasa.gov/resource/gquh-watm.json")
+    data = response.json()
 
-# Convert each item in new_data to a Patent object
-patent_objects = [Patent(**item) for item in new_data]
+    new_data = clean_date(data)
 
-# Convert the list of Patent objects to a list of dictionaries using model_dump()
-dict_data = [patent.model_dump() for patent in patent_objects]
+    # Convert each item in new_data to a Patent object
+    patent_objects = [Patent(**item) for item in new_data]
 
-# Convert the list of dictionaries to a DataFrame
-data_df = pd.DataFrame(dict_data)
+    # Convert the list of Patent objects to a list of dictionaries using model_dump()
+    dict_data = [patent.model_dump() for patent in patent_objects]
 
-# Save the DataFrame to a CSV file
-csv_path = 'my_data.csv'
-data_df.to_csv(csv_path, index=False)
+    # Convert the list of dictionaries to a DataFrame
+    data_df = pd.DataFrame(dict_data)
 
-print("File saved")
+    # Save the DataFrame to a CSV file
+    csv_path = 'my_data.csv'
+    data_df.to_csv(csv_path, index=False)
+
+    print("File created")
+
+    # Save CSV file in the client machine
+    return send_file(csv_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
