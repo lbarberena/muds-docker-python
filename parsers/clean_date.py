cc87fa8dd676f11f5a4a6945b533491b9115837c
lbarberena <octaviobarberena@hotmail.com>
2023-12-01 00:52:52 +0100

    Edit clean_date function to use Patent class as typing

--- parsers/clean_date.py
+++ parsers/clean_date.py
@@ -1,12 +1,15 @@
-def clean_date(dataToClean):
+from typing import List
+from models.patents_md import Patent
+
+def clean_date(dataToClean: List[Patent]):
     for item in dataToClean:
         # Check if the 'patent_expiration_date' field exists in the object
-        if "patent_expiration_date" in item:
+        if hasattr(item, "patent_expiration_date"):
             # Extracting the date part from the ISO format
-            iso_date = item["patent_expiration_date"].split("T")[0]
+            iso_date = item.patent_expiration_date.split("T")[0]
             # Assigning the date part back to the patent_expiration_date field
-            item["patent_expiration_date"] = iso_date
+            item.patent_expiration_date = iso_date
         else:
-            item["patent_expiration_date"] = "UNKNOWN"
+            item.patent_expiration_date = "UNKNOWN"
 
     return dataToClean
